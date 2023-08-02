b53fe518e9277da88c34361f9355f9de3d02b308
Nicholas Myers <nic338@gmail.com>
2023-08-02 10:16:23 -0400

    changed map to dynamically scale sizing based on the aspect ratio of the image

--- LegendLore/client/legendlore/src/components/Campaigns/CampaignMap.js
+++ LegendLore/client/legendlore/src/components/Campaigns/CampaignMap.js
@@ -1,48 +1,33 @@
-import { MapContainer, TileLayer, Marker, Popup, ImageOverlay } from "react-leaflet"
+import { MapContainer, ImageOverlay, useMap } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
-import { useEffect, useRef, useState } from "react";
-import { Icon } from "leaflet";
+import { useEffect, useState } from "react";
 import { getMapById } from "../../Managers/MapManager";
 import { useParams } from "react-router-dom";
-import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MapImageOverlay } from "./MapImageOverlay";
 
 export const CampaignMap = () => {
     const [mapObject, setMapObject] = useState({});
     const [mapPOIs, setMapPOIs] = useState([]);
     const { mapId } = useParams();
     const referencePoint = [50, -100];
-    const pixelsPerUnit = 3;
-    
+
     useEffect(() => {
         getMapById(mapId)
         .then((map) => {
             setMapObject(map)
-            console.log(imageWidth)
-            console.log(imageHeight)
         })
     }, [mapId])
-    
-    const imageWidth = mapObject.width;
-    const imageHeight = mapObject.height;
-    const aspectRatio = imageWidth / imageHeight;
-    const topLeftLatLng = [
-        referencePoint[0] + (0 - imageHeight / 2) / pixelsPerUnit,
-        referencePoint[1] + (0 - imageWidth / 2) / (pixelsPerUnit * aspectRatio),
-      ];
-      const bottomRightLatLng = [
-        referencePoint[0] + (imageHeight / 2) / pixelsPerUnit,
-        referencePoint[1] + (imageWidth / 2) / (pixelsPerUnit * aspectRatio),
-      ];
-    const bounds = [topLeftLatLng, bottomRightLatLng]
+
     if (!mapObject) {
         return <div>LOADING....</div>
     }
     else {
+        //moved ImageOverlay to a child component because useMap can only be used in a component wrapped within a MapContainer
         return (
             <MapContainer id="mapId" zoom={3} center={referencePoint}>
-                {mapObject && mapObject.mapImage && (
-                    <ImageOverlay url={mapObject.mapImage} bounds={bounds} />
+                {mapObject && mapObject.mapImage && mapObject.width && mapObject.height && (
+                    <MapImageOverlay mapObject={mapObject}/>
                 )}
             </MapContainer>
         );
---
+++ LegendLore/client/legendlore/src/components/Campaigns/MapImageOverlay.js
@@ -0,0 +1,38 @@
+import { ImageOverlay, useMap } from "react-leaflet";
+
+export const MapImageOverlay = ({ mapObject }) => {
+    const map = useMap();
+    const imageWidth = mapObject.width;
+    const imageHeight = mapObject.height;
+    //aspect ratio of the map image we are using
+    const aspectRatio = imageWidth / imageHeight;
+    //variables used to calculate the lat/lng ranges of the visable map area
+    //can only use these when useMap is imported
+    const latViewRange = map.getBounds().getNorth() - map.getBounds().getSouth();
+    const lngViewRange = map.getBounds().getEast() - map.getBounds().getWest();
+    //aspect ratio of the visable map area
+    const mapViewAspectRatio = lngViewRange / latViewRange;
+
+    // conditional that will change the width and height based on which is larger
+    //if it is wider than it is tall, use the full width and adjust the height
+    //if it is taller than it is wide, use the full height and adjust the width
+    let overlayWidth = null;
+  let overlayHeight = null;
+  if (mapViewAspectRatio > aspectRatio) {
+    overlayWidth = lngViewRange;
+    overlayHeight = overlayWidth / aspectRatio;
+  } else {
+    overlayHeight = latViewRange;
+    overlayWidth = overlayHeight * aspectRatio;
+  }
+
+    // grabs the center lat/lng of the map view
+    const centerLat = map.getCenter().lat;
+    const centerLng = map.getCenter().lng;
+    //calculate the bounds of the map image based on everything above
+    const topLeftLatLng = [centerLat + overlayHeight / 2, centerLng - overlayWidth / 2];
+    const bottomRightLatLng = [centerLat - overlayHeight / 2, centerLng + overlayWidth / 2];
+    const bounds = [topLeftLatLng, bottomRightLatLng];
+    
+    return <ImageOverlay url={mapObject.mapImage} bounds={bounds} />
+}
